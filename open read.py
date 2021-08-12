7877a41a091bc918f085b9ba04ad6851397f293d
Ismail Hossain <88856157+Codewins2@users.noreply.github.com>
2021-08-12 13:14:41 -0700

    Add files via upload

---
+++ open read.py
@@ -0,0 +1,15 @@
+"""
+f = open("harry.txt", "rt")
+print(f.readlines())
+content = f.read()
+for line in f:
+    print(line)
+print(f.readline()) # to read lines and it counts line one by one.depends how many times you print that
+pritn(f.readline())
+
+
+f.close()
+
+readlines 
+"""
+
